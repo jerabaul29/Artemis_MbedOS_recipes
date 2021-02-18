487dc0d3070bcc80d17f93364f0022cb1d7a33fc
JRT <jean.rblt@gmail.com>
2021-02-18 20:04:54 +0100

    Minor printout updates

--- recipes/recipe_IMU_quaternions/recipe_IMU_quaternions.ino
+++ recipes/recipe_IMU_quaternions/recipe_IMU_quaternions.ino
@@ -19,6 +19,7 @@ void printAccuracyLevel(byte accuracyNumber){          // accuracy is:
 // some parameters
 constexpr unsigned long imu_output_period_millis = 500UL;
 unsigned long millis_last_analysis {0};
+constexpr bool verbose_loop {false};
 
 //------------------------------------------------------------------------
 void setup() {
@@ -77,14 +78,18 @@ void loop() {
   uint16_t reading_status = bno080_imu.getReadings();
   
   if (reading_status != 0){
-    Serial.print(F("received data at ms: ")); Serial.print(millis()); Serial.print(F(" | type: "));
+    if (verbose_loop){
+      Serial.print(F("received data at ms: ")); Serial.print(millis()); Serial.print(F(" | type: "));
+    }
 
     if (reading_status == SENSOR_REPORTID_ACCELEROMETER){
       accel_x = bno080_imu.getAccelX();
       accel_y = bno080_imu.getAccelY();
       accel_z = bno080_imu.getAccelZ();
       accel_accuracy = bno080_imu.getLinAccelAccuracy();
-      Serial.println(F("accel"));
+      if (verbose_loop){
+        Serial.println(F("accel"));
+      }
     }
 
     if(reading_status == SENSOR_REPORTID_GYROSCOPE){
@@ -92,7 +97,9 @@ void loop() {
       gyro_y = bno080_imu.getGyroY();
       gyro_z = bno080_imu.getGyroZ();
       gyro_accuracy = bno080_imu.getGyroAccuracy();
-      Serial.println(F("gyro"));
+      if (verbose_loop){
+        Serial.println(F("gyro"));
+      }
     }
 
     if(reading_status == SENSOR_REPORTID_MAGNETIC_FIELD){
@@ -100,7 +107,9 @@ void loop() {
       mag_y = bno080_imu.getMagY();
       mag_z = bno080_imu.getMagZ();
       mag_accuracy = bno080_imu.getMagAccuracy();
-      Serial.println(F("mag"));
+      if (verbose_loop){
+        Serial.println(F("mag"));
+      }
     }
 
     if(reading_status == SENSOR_REPORTID_ROTATION_VECTOR){
@@ -110,7 +119,9 @@ void loop() {
       quat_real = bno080_imu.getQuatReal();
       quat_accuracy = bno080_imu.getQuatAccuracy();
       quat_radian_accuracy = bno080_imu.getQuatRadianAccuracy();
-      Serial.println(F("quat"));
+      if (verbose_loop){
+        Serial.println(F("quat"));
+      }
     }
   }
 
@@ -125,91 +136,102 @@ void loop() {
     millis_last_analysis += imu_output_period_millis;
     unsigned long millis_start = millis();
 
-    Serial.println();
-    Serial.print(F("millis start analysis: ")); Serial.println(millis_start);
-
-    Serial.print(F("accel: "));
-    Serial.print(accel_x, 4);
-    Serial.print(F(", "));
-    Serial.print(accel_y, 4);
-    Serial.print(F(", "));
-    Serial.print(accel_z, 4);
-    Serial.print(F(", pres: "));
-    printAccuracyLevel(accel_accuracy);
-
-    Serial.print(F(" | gyro: "));
-    Serial.print(gyro_x, 4);
-    Serial.print(F(", "));
-    Serial.print(gyro_y, 4);
-    Serial.print(F(", "));
-    Serial.print(gyro_z, 4);
-    Serial.print(F(", pres: "));
-    printAccuracyLevel(gyro_accuracy);
-
-    Serial.print(F(" | mag: "));
-    Serial.print(mag_x, 4);
-    Serial.print(F(", "));
-    Serial.print(mag_y, 4);
-    Serial.print(F(", "));
-    Serial.print(mag_z, 4);
-    Serial.print(F(", pres: "));
-    printAccuracyLevel(mag_accuracy);
-    
-    Serial.print(F(" | quat: "));
-    Serial.print(quat_i, 4);
-    Serial.print(F(", "));
-    Serial.print(quat_j, 4);
-    Serial.print(F(", "));
-    Serial.print(quat_k, 4);
-    Serial.print(F(", "));
-    Serial.print(quat_real, 4);
-    Serial.print(F(", pres: "));
-    printAccuracyLevel(quat_accuracy);
-    Serial.print(F(", "));
-    Serial.print(quat_radian_accuracy, 4);
-    Serial.println();
+    if (verbose_loop){
+      Serial.println();
+      Serial.print(F("millis start analysis: ")); Serial.println(millis_start);
+  
+      Serial.print(F("accel: "));
+      Serial.print(accel_x, 4);
+      Serial.print(F(", "));
+      Serial.print(accel_y, 4);
+      Serial.print(F(", "));
+      Serial.print(accel_z, 4);
+      Serial.print(F(", pres: "));
+      printAccuracyLevel(accel_accuracy);
+  
+      Serial.print(F(" | gyro: "));
+      Serial.print(gyro_x, 4);
+      Serial.print(F(", "));
+      Serial.print(gyro_y, 4);
+      Serial.print(F(", "));
+      Serial.print(gyro_z, 4);
+      Serial.print(F(", pres: "));
+      printAccuracyLevel(gyro_accuracy);
+  
+      Serial.print(F(" | mag: "));
+      Serial.print(mag_x, 4);
+      Serial.print(F(", "));
+      Serial.print(mag_y, 4);
+      Serial.print(F(", "));
+      Serial.print(mag_z, 4);
+      Serial.print(F(", pres: "));
+      printAccuracyLevel(mag_accuracy);
+      
+      Serial.print(F(" | quat: "));
+      Serial.print(quat_i, 4);
+      Serial.print(F(", "));
+      Serial.print(quat_j, 4);
+      Serial.print(F(", "));
+      Serial.print(quat_k, 4);
+      Serial.print(F(", "));
+      Serial.print(quat_real, 4);
+      Serial.print(F(", pres: "));
+      printAccuracyLevel(quat_accuracy);
+      Serial.print(F(", "));
+      Serial.print(quat_radian_accuracy, 4);
+      Serial.println();
+    }
 
     // look at quaternion data
     Quaternion quat_orientation {quat_real, quat_i, quat_j, quat_k};
-    print(quat_orientation);
-    Serial.print(F("quat norm: "));
-    Serial.println(quat_orientation.norm());
+    if (verbose_loop){
+      print(quat_orientation);
+      Serial.print(F("quat norm: "));
+      Serial.println(quat_orientation.norm());
+    }
 
     if (abs(quat_orientation.norm() - 1.0f) > 1.0e-4){
       Serial.println(F("**********************************************************************"));
       Serial.println(F("************************ ERROR: NON UNIT QUAT ************************"));
       Serial.println(F("**********************************************************************"));
+      Serial.print(F("quat norm: ")); Serial.println(quat_orientation.norm());
     }
 
     // acceleration in an IMU and ENU referential (the datasheet says it outputs in East North Up rather than North East Down)
     Vector accel_imu_ref{accel_x, accel_y, accel_z};
-    Serial.print(F("accel norm IMU frame of ref: ")); Serial.print(accel_imu_ref.norm());
+    if (verbose_loop){
+      Serial.print(F("accel norm IMU frame of ref: ")); Serial.print(accel_imu_ref.norm());
+    }
 
     Vector accel_ENU_ref{};
     rotate_vect_by_quat_R(accel_imu_ref, quat_orientation, accel_ENU_ref);
-    Serial.print(F(" | ENU frame of ref: ")); Serial.println(accel_ENU_ref.norm());
-    Serial.print(F("accel ENU: "));
-    print(accel_ENU_ref);
+    if (verbose_loop){
+      Serial.print(F(" | ENU frame of ref: ")); Serial.println(accel_ENU_ref.norm());
+      Serial.print(F("accel ENU: "));
+      print(accel_ENU_ref);
+    }
 
     // where is the X-direction of the IMU pointing?
     Vector imu_dir_x_ref_imu{1, 0, 0};
     Vector imu_dir_x_ref_enu{};
     rotate_vect_by_quat_R(imu_dir_x_ref_imu, quat_orientation, imu_dir_x_ref_enu);
-    Serial.print(F("IMU X-dir in ENU frame: "));
-    print(imu_dir_x_ref_enu);
+    if (verbose_loop){
+      Serial.print(F("IMU X-dir in ENU frame: "));
+      print(imu_dir_x_ref_enu);
+    }
 
-    // TODO: quality checks
+    // quality checks
     if (abs(accel_ENU_ref.norm() - 9.81) > 10.0){
       Serial.println(F("**************************************************"));
       Serial.println(F("***** WARNING: VERY HIGH ACCELS, IS IT TRUE? *****"));
       Serial.println(F("**************************************************"));
+      Serial.print(F("accel vect: ")); print(accel_ENU_ref);
     }
 
     unsigned long millis_end = millis();
-    Serial.print(F("millis end: ")); Serial.print(millis_end); Serial.print(F(" | analysis + print duration: ")); Serial.print(millis_end-millis_start); Serial.println(F("ms"));
-    Serial.println();
-    
+    if (verbose_loop){
+      Serial.print(F("millis end: ")); Serial.print(millis_end); Serial.print(F(" | analysis + print duration: ")); Serial.print(millis_end-millis_start); Serial.println(F("ms"));
+      Serial.println();
+    }
   }
 }
-
-// TODO: quat norm is sometimes off!
