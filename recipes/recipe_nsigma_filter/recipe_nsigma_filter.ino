16563fc17da91a527462034908e12f8395e48367
JRT <jean.rblt@gmail.com>
2021-05-13 10:59:36 +0200

    Fixes to the sigma filtering

--- recipes/recipe_nsigma_filter/recipe_nsigma_filter.ino
+++ recipes/recipe_nsigma_filter/recipe_nsigma_filter.ino
@@ -33,9 +33,50 @@ void setup(){
   vec_in.push_back(value -18);
   vec_in.push_back(value +4);
   vec_in.push_back(value -7);
-  long val_out = sigma_filter(vec_in, 2.0);
+  double val_out = sigma_filter(vec_in, 2.0);
   Serial.print(F("val_out=")); Serial.println(val_out);
   }
+
+  {
+    /*
+  unsigned long value = 12345678;
+  Serial.print(F("test around ")); Serial.print(value); Serial.print(F(": "));
+  etl::vector<unsigned long, 5> vec_in;
+  vec_in.push_back(value -15UL);
+  vec_in.push_back(value -10UL);
+  vec_in.push_back(value +0UL);
+  vec_in.push_back(value +10UL);
+  vec_in.push_back(value +15UL);
+  long val_out = sigma_filter(vec_in, 2.0);
+  Serial.print(F("val_out=")); Serial.println(val_out);
+    */
+  }
+
+  {
+  int value = 12345678;
+  Serial.print(F("test around ")); Serial.print(value); Serial.print(F(": "));
+  etl::vector<int, 5> vec_in;
+  vec_in.push_back(value -15);
+  vec_in.push_back(value -10);
+  vec_in.push_back(value +0);
+  vec_in.push_back(value +10);
+  vec_in.push_back(value +15);
+  int val_out = sigma_filter(vec_in, 2.0);
+  Serial.print(F("val_out=")); Serial.println(val_out);
+  }
+{
+  long value = 12345678;
+  Serial.print(F("test with outlier ")); Serial.print(value); Serial.print(F(": "));
+  etl::vector<long, 5> vec_in;
+  vec_in.push_back(value -5);
+  vec_in.push_back(value -10);
+  vec_in.push_back(value +100000);
+  vec_in.push_back(value +10);
+  vec_in.push_back(value +15);
+  long val_out = sigma_filter(vec_in, 2.0, false);
+  Serial.print(F("val_out=")); Serial.println(val_out);
+  }
+
 }
 
 void loop(){
--- recipes/recipe_nsigma_filter/statistical_processing.h
+++ recipes/recipe_nsigma_filter/statistical_processing.h
@@ -10,24 +10,25 @@
 
 template <typename T>
 T sigma_filter(etl::ivector<T> const & vec_in, float n_sigma=3, bool verbose=false){
-    static_assert(std::is_signed<T>::value, "signed values only");
+        static_assert(std::is_signed<T>::value, "signed values only");
 
+    // we first take a coarse mean, in such a way that we cannot overflow
     T mean_coarse {0};
 
     for (size_t index=0; index<vec_in.size(); index++){
-        mean_coarse += vec_in[index] / static_cast<T>(vec_in.size());
+            mean_coarse += vec_in[index] / static_cast<T>(vec_in.size());
     }
 
     if (verbose){
-      Serial.print(F("mean_coarse: ")); Serial.println(mean_coarse);
+          Serial.print(F("mean_coarse: ")); Serial.println(mean_coarse);
     }
 
-    // then we need to compute the mean and mean of square
+    // then we need to compute the mean and mean of square when we have removed the coarse mean
     T mean{0};
     T mean_of_squares {0};
 
     for (size_t index=0; index<vec_in.size(); index++){
-        mean += vec_in[index] - mean_coarse;
+            mean += vec_in[index] - mean_coarse;
         mean_of_squares += (vec_in[index] - mean_coarse) * (vec_in[index] - mean_coarse);
     }
 
@@ -35,55 +36,62 @@ T sigma_filter(etl::ivector<T> const & vec_in, float n_sigma=3, bool verbose=fal
     mean_of_squares /= vec_in.size();
 
     if (verbose){
-      Serial.print(F("mean (without the coarse part): ")); Serial.println(mean);
-      Serial.print(F("mean_of_squares (witout the coarse part): ")); Serial.println(mean_of_squares);
+          Serial.print(F("mean (without the coarse part): ")); Serial.println(mean);
+          Serial.print(F("mean_of_squares (witout the coarse part): ")); Serial.println(mean_of_squares);
     }
 
-    // compute the std estimate 
+    // now we can compute the std estimate 
     T std_estimate {0};
 
     std_estimate = static_cast<T>(std::sqrt(static_cast<double>(
-        mean_of_squares - mean * mean
+            mean_of_squares - mean * mean
     )));
     
     if (verbose){
-      Serial.print(F("std_estimate: ")); Serial.println(std_estimate);
+          Serial.print(F("std_estimate: ")); Serial.println(std_estimate);
     }
 
     // now compute the filtered mean, with a n_sigma filter
+
+    // just the number of sigmas we want to use, as a T type
     T n_sigma_native = static_cast<T>(n_sigma);
 
-    if (n_sigma_native == 0){
-      return (mean + mean_coarse);
+    if ((n_sigma_native == 0) || (std_estimate == 0)){
+          return (mean + mean_coarse);
     }
 
+    // compute the filtered mean
     T filtered{0};
     int nbr_valid {0};
 
     for (size_t index=0; index<vec_in.size(); index++){
-        T crrt_entry = vec_in[index] - mean_coarse;
+            T crrt_entry = vec_in[index] - mean_coarse;
         if (verbose){
-          Serial.print(F("crrt_entry: ")); Serial.println(crrt_entry);
+              Serial.print(F("crrt_entry: ")); Serial.println(crrt_entry);
         }
 
         // do we have a valid value?
         if (crrt_entry - mean <= n_sigma_native * std_estimate){
-            
+                
             if (verbose){
-              Serial.println(F("we use it"));
+                  Serial.println(F("we use it"));
             }
             nbr_valid += 1;
             filtered += crrt_entry;
         }
 
         if (verbose){
-          Serial.print(F("filtered: ")); Serial.println(filtered);
+              Serial.print(F("filtered non normalized: ")); Serial.println(filtered);
         }
 
     }
 
     if (verbose){
-      Serial.print(F("nbr_valid: ")); Serial.println(nbr_valid);
+          Serial.print(F("nbr_valid: ")); Serial.println(nbr_valid);
+    }
+
+    if (nbr_valid == 0){
+      return (mean + mean_coarse);
     }
 
     filtered = filtered / nbr_valid;
